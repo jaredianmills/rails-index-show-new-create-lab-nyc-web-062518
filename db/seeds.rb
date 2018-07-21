a6005963f4475270af863f5bb344311908ffb7f2
jaredianmills <jaredianmills@gmail.com>
2018-07-21 11:36:36 -0400

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "83kakjdfkj91", store: "Target")
+Coupon.create(coupon_code: "aldjfakldjf887", store: "Chipotle")
+Coupon.create(coupon_code: "8347aldkjff1", store: "Barnes & Noble")
+Coupon.create(coupon_code: "84749fjdjf", store: "Whole Foods")
+Coupon.create(coupon_code: "8373000dfj", store: "Hermes")
